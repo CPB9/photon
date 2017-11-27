b30613d219abc537747466433a9f4bb99810fdde
Skorokhod Sergey <skorokhod.sergey@gmail.com>
2017-11-27 15:11:21 +0300

    default reaction for grp commands

--- modules/photon/grp/Grp.c
+++ modules/photon/grp/Grp.c
@@ -5,8 +5,30 @@
 
 #ifdef PHOTON_STUB
 
+void clearState()
+{
+    _photonGrp.commit = 0;
+    _photonGrp.group.type = PhotonOptionGrpGrpIdType_None;
+    _photonGrp.lastLogIdx = 0;
+    _photonGrp.lastLogTerm = 0;
+    _photonGrp.leader.type = PhotonOptionGrpUavIdType_None;
+    _photonGrp.members.size = 0;
+    _photonGrp.state = 0;
+    _photonGrp.term = 0;
+    _photonGrp.votedFor.type = PhotonOptionGrpUavIdType_None;
+}
+
 void PhotonGrp_Init()
 {
+    _photonGrp.commit = 0;
+    _photonGrp.group.type = PhotonOptionGrpGrpIdType_None;
+    _photonGrp.lastLogIdx = 0;
+    _photonGrp.lastLogTerm = 0;
+    _photonGrp.leader.type = PhotonOptionGrpUavIdType_None;
+    _photonGrp.members.size = 0;
+    _photonGrp.state = PhotonGrpMemberState_Follower;
+    _photonGrp.term = 0;
+    _photonGrp.votedFor.type = PhotonOptionGrpUavIdType_None;
 }
 
 void PhotonGrp_Tick()
@@ -21,10 +43,16 @@ PhotonError PhotonGrp_SetGroupAddress(uint64_t address)
 
 PhotonError PhotonGrp_CreateGroup(uint64_t group, PhotonDynArrayOfGrpUavIdMaxSize10 const* members)
 {
-    PHOTON_DEBUG("CreateGroup: group(%" PRIu64 "), count(%" PRIu64 ")", group, members->size);
-    for(uint64_t i = 0; i < members->size; ++i)
+    clearState();
+    _photonGrp.group.type = PhotonOptionGrpGrpIdType_Some;
+    _photonGrp.group.data.someOptionGrpGrpId._1 = group;
+    _photonGrp.members.size = members->size;
+
+    PHOTON_DEBUG("CreateGroup: group(%" PRIu64 "), count(%" PRIu64 ")", group, _photonGrp.members.size);
+    for(uint64_t i = 0; i < _photonGrp.members.size; ++i)
     {
-        PHOTON_DEBUG("CreateGroup: group(%" PRIu64 "), index(%" PRIu64 "), member(%" PRIu64 ")", group, i, members->data[i]);
+        _photonGrp.members.data[i] = members->data[i];
+        PHOTON_DEBUG("CreateGroup: group(%" PRIu64 "), index(%" PRIu64 "), member(%" PRIu64 ")", group, i, _photonGrp.members.data[i]);
     }
     return PhotonError_Ok;
 }
@@ -32,17 +60,29 @@ PhotonError PhotonGrp_CreateGroup(uint64_t group, PhotonDynArrayOfGrpUavIdMaxSiz
 PhotonError PhotonGrp_DeleteGroup(uint64_t group)
 {
     PHOTON_DEBUG("DeleteGroup: group(%" PRIu64 ")", group);
+    clearState();
     return PhotonError_Ok;
 }
 
 PhotonError PhotonGrp_AddMember(uint64_t group, uint64_t member, PhotonGrpReqCfgRep* rv)
 {
+    if (_photonGrp.group.type == PhotonOptionGrpGrpIdType_None || _photonGrp.group.data.someOptionGrpGrpId._1 != group)
+        return PhotonError_InvalidDeviceId;
+    if (_photonGrp.members.size >= sizeof(_photonGrp.members.data)/sizeof(_photonGrp.members.data[0]))
+        return PhotonError_InvalidSize;
+    _photonGrp.members.data[_photonGrp.members.size] = member;
+    _photonGrp.members.size++;
     PHOTON_DEBUG("AddMember: group(%" PRIu64 "), member(%" PRIu64 ")", group, member);
     return PhotonError_Ok;
 }
 
 PhotonError PhotonGrp_RemoveMember(uint64_t group, uint64_t member, PhotonGrpReqCfgRep* rv)
 {
+    if (_photonGrp.group.type == PhotonOptionGrpGrpIdType_None || _photonGrp.group.data.someOptionGrpGrpId._1 != group)
+        return PhotonError_InvalidDeviceId;
+    if (_photonGrp.members.size == 0)
+        return PhotonError_InvalidSize;
+    _photonGrp.members.size--;
     PHOTON_DEBUG("RemoveMember: group(%" PRIu64 "), member(%" PRIu64 ")", group, member);
     return PhotonError_Ok;
 }
