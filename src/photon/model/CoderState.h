fbc6f69ed01b7843a256f34df8a84c85853f078e
dndanik <dndanik@gmail.com>
2017-11-28 11:55:07 +0300

    Fix link errors

--- src/photon/model/CoderState.h
+++ src/photon/model/CoderState.h
@@ -4,11 +4,24 @@
 #include <bmcl/MemReader.h>
 #include <bmcl/Buffer.h>
 
+#include <string>
+
 namespace photon {
 
 class CoderState {
 public:
-    void setError(bmcl::StringView msg);
+    void setError(bmcl::StringView msg)
+    {
+        _error.assign(msg.begin(), msg.end());
+    }
+
+    const std::string error() const
+    {
+        return _error;
+    }
+
+private:
+    std::string _error;
 };
 }
 
