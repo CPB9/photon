b5d35ea3a863eb5945de646aae99ff7813dfdd65
Skorokhod Sergey <skorokhod.sergey@gmail.com>
2017-12-07 17:55:12 +0300

    no grp tm when group is not enabled

--- modules/photon/grp/Grp.c
+++ modules/photon/grp/Grp.c
@@ -63,6 +63,13 @@ PhotonError PhotonGrp_CreateGroup(uint64_t group, PhotonDynArrayOfGrpUavIdMaxSiz
         _photonGrp.members.data[i] = members->data[i];
         PHOTON_INFO("CreateGroup: group(%" PRIu64 "), index(%" PRIu64 "), member(%" PRIu64 ")", group, i, _photonGrp.members.data[i]);
     }
+
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_ALL_ID, true);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_LEADER_ID, true);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_MEMBERS_ID, true);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_ELECT_ID, true);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_NOGROUP_ID, true);
+
     return PhotonError_Ok;
 }
 
@@ -73,6 +80,13 @@ PhotonError PhotonGrp_DeleteGroup(uint64_t group)
 
     PHOTON_INFO("DeleteGroup: group(%" PRIu64 ")", group);
     clearState();
+
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_ALL_ID, false);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_LEADER_ID, false);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_MEMBERS_ID, false);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_ELECT_ID, false);
+    PhotonTm_SetStatusEnabled(PHOTON_GRP_COMPONENT_ID, PHOTON_GRP_STATUS_NOGROUP_ID, true);
+
     return PhotonError_Ok;
 }
 
