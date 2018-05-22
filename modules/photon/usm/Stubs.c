0523d47e73370383a71eb15b91888999dc288273
Dmitry Voronkin <dmitry@voronkin.ru>
2018-05-22 19:37:19 +0300

    Usm stubs bugfix.

--- modules/photon/usm/Stubs.c
+++ modules/photon/usm/Stubs.c
@@ -2,77 +2,21 @@
 
 #ifdef PHOTON_STUB
 
-void PhotonUsm_Init()
-{
-}
+void PhotonUsm_Init() {}
+void PhotonUsm_Tick() {}
 
-void PhotonUsm_Tick()
-{
-}
-
-PhotonError PhotonUsm_ExecCmd_UpdateInfo(PhotonUsmLanguageCodes language, uint8_t statusCode)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_ConfirmReceiving(PhotonUsmLanguageCodes language, PhotonUsmFrames frame)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_GetFile(PhotonUsmLanguageCodes language, uint64_t fileId)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_UnpackFile(PhotonUsmLanguageCodes language)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_BuildTarget(PhotonUsmLanguageCodes language)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_InstallTarget(PhotonUsmLanguageCodes language)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_StopBuildProcess(PhotonUsmLanguageCodes language, bool kill)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_UninstallApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_RunApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize768* appNameWithArguments)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_StopApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_KillApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_RequestApplicationStatus(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName)
-{
-    return PhotonError_Ok;
-}
-
-PhotonError PhotonUsm_ExecCmd_RequestProjectOfApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName)
-{
-    return PhotonError_Ok;
-}
+PhotonError PhotonUsm_ExecCmd_UpdateInfo(PhotonUsmLanguageCodes language, PhotonUsmFrames frame) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_ConfirmReceiving(PhotonUsmLanguageCodes language, PhotonUsmFrames frame) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_GetFile(PhotonUsmLanguageCodes language, uint64_t fileId) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_UnpackFile(PhotonUsmLanguageCodes language) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_BuildTarget(PhotonUsmLanguageCodes language) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_InstallTarget(PhotonUsmLanguageCodes language) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_StopBuildProcess(PhotonUsmLanguageCodes language, bool kill) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_UninstallApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_RunApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize768* appNameWithArguments) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_StopApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_KillApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_RequestApplicationStatus(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName) { return PhotonError_NotImplemented; }
+PhotonError PhotonUsm_ExecCmd_RequestProjectOfApplication(PhotonUsmLanguageCodes language, const PhotonDynArrayOfCharMaxSize255* appName) { return PhotonError_NotImplemented; }
 
 #endif
