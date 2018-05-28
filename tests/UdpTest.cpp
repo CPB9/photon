6fde9bf7a4faf9753771c68acb573d05f35c1f1e
dndanik <dndanik@gmail.com>
2018-05-28 17:44:41 +0300

    Add generic client

--- tests/UdpTest.cpp
+++ tests/UdpTest.cpp
@@ -1,120 +1,16 @@
 #include "UiTest.h"
+#include "UdpStream.h"
 
-#include <decode/core/Rc.h>
-#include <photon/groundcontrol/Exchange.h>
-#include <photon/groundcontrol/FwtState.h>
-#include <photon/groundcontrol/Atoms.h>
 #include <photon/groundcontrol/AllowUnsafeMessageType.h>
-
-#include <bmcl/Logging.h>
-#include <bmcl/SharedBytes.h>
-
-#include <asio/ip/udp.hpp>
 
 #include <tclap/CmdLine.h>
 
-#include <caf/send.hpp>
-#include <caf/others.hpp>
-#include <caf/blocking_actor.hpp>
-
 using namespace photon;
-using asio::ip::udp;
 
 DECODE_ALLOW_UNSAFE_MESSAGE_TYPE(bmcl::SharedBytes);
 
-class UdpStream : public caf::blocking_actor {
-public:
-    UdpStream(caf::actor_config& cfg, udp::socket&& socket, udp::endpoint&& endpoint)
-        : caf::blocking_actor(cfg)
-        , _socket(std::move(socket))
-        , _endpoint(std::move(endpoint))
-        , _isRunning(true)
-
-    {
-        _socket.non_blocking(true);
-    }
-
-    void act() override
-    {
-        asio::error_code err;
-        std::size_t size = _socket.send_to(asio::buffer("", 0), _endpoint, 0, err);
-        do {
-            if (has_next_message()) {
-                receive(
-                    [this](SendDataAtom, const bmcl::SharedBytes& data) {
-                        asio::error_code err;
-                        std::size_t size = _socket.send_to(asio::buffer(data.data(), data.size()), _endpoint, 0, err);
-                        if (err) {
-                            BMCL_CRITICAL() << "error sending packet: " << err.message();
-                        }
-                        assert(size == data.size());
-                    },
-                    [this](SetStreamDestAtom, const caf::actor& actor) {
-                        _dest = actor;
-                    },
-                    [this](StartAtom) {
-                        _hasStarted = true;
-                    },
-                    [&](const caf::down_msg& x) {
-                        _isRunning = false;
-                    },
-                    [&](const caf::exit_msg& x) {
-                        _isRunning = false;
-                    },
-                    caf::others >> [](caf::message_view& x) -> caf::result<caf::message> {
-                        return caf::sec::unexpected_message;
-                    }
-                );
-            }
-
-            if (!_hasStarted) {
-                continue;
-            }
-
-            recieveFromSocket();
-        } while (_isRunning);
-    }
-
-    void recieveFromSocket()
-    {
-        std::array<uint8_t, 2048> buf;
-        asio::error_code err;
-        udp::endpoint endpoint;
-        std::size_t size = _socket.receive_from(asio::buffer(buf), endpoint, 0, err);
-        if (err) {
-            if (err == asio::error::would_block) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                return;
-            }
-            BMCL_CRITICAL() << "error recieving packet: " << err.message();
-            return;
-        }
-        if (endpoint != _endpoint) {
-            BMCL_WARNING() << "recieved packet from different endpoint";
-            return;
-        }
-        if (size == 0) {
-            return;
-        }
-
-        bmcl::SharedBytes data = bmcl::SharedBytes::create(buf.data(), size);
-        send(_dest, RecvDataAtom::value, data);
-    }
-
-    void on_exit() override
-    {
-        send_exit(_dest, caf::exit_reason::user_shutdown);
-        destroy(_dest);
-    }
-
-    bool _hasStarted;
-    caf::actor _dest;
-    udp::socket _socket;
-    udp::endpoint _endpoint;
-    bool _isRunning;
-};
-
 using namespace decode;
+using asio::ip::udp;
 
 int main(int argc, char** argv)
 {
