d11a5cbb32cfee5f9374161633dc3c8167b4ca3c
Sergey Skorokhod <skorokhod.sergey@gmail.com>
2018-05-30 23:49:48 +0300

    the way to set work dir for usm

--- modules/photon/usm/Stubs.c
+++ modules/photon/usm/Stubs.c
@@ -4,6 +4,7 @@
 
 void PhotonUsm_Init() {}
 void PhotonUsm_Tick() {}
+void PhotonUsm_SetDir(const char* dir) {}
 
 PhotonError PhotonUsm_ExecCmd_UpdateInfo(PhotonUsmLanguageCodes language, PhotonUsmFrames frame) { return PhotonError_NotImplemented; }
 PhotonError PhotonUsm_ExecCmd_UpdateInfoMultiple(PhotonUsmLanguageCodes language, uint64_t frames) { return PhotonError_NotImplemented; }
