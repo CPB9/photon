17076a5665ae3f8667c1c13db9d6e80b7d6bb166
dndanik <dndanik@gmail.com>
2018-06-04 22:09:32 +0300

    Add fcu stubs

--- modules/photon/fcu/Fcu.c
+++ modules/photon/fcu/Fcu.c
@@ -4,7 +4,7 @@
 
 #define _PHOTON_FNAME "fcu/Fcu.c"
 
-extern void setArmed();
+extern void setArmed(bool flag);
 
 void PhotonFcu_Init()
 {
@@ -25,5 +25,49 @@ PhotonError PhotonFcu_ExecCmd_Disarm()
     setArmed(false);
     return PhotonError_Ok;
 }
+
+#ifdef PHOTON_STUB
+
+void setArmed(bool flag)
+{
+
+}
+
+PhotonError PhotonFcu_ExecCmd_StartSensorCalibration(PhotonFcuSensors sensor)
+{
+    return PhotonError_Ok;
+}
+
+PhotonError PhotonFcu_ExecCmd_CancelSensorCalibration()
+{
+    return PhotonError_Ok;
+}
+
+PhotonError PhotonFcu_ExecCmd_WriteRadioCalibrationRatios(PhotonFcuRcRatio rcRatios[18], PhotonFcuRcCalFunction calFunctions[4])
+{
+    return PhotonError_Ok;
+}
+
+PhotonError PhotonFcu_ExecCmd_SetActive(bool active)
+{
+    return PhotonError_Ok;
+}
+
+PhotonError PhotonFcu_ExecCmd_TakeOff()
+{
+    return PhotonError_Ok;
+}
+
+PhotonError PhotonFcu_ExecCmd_Land()
+{
+    return PhotonError_Ok;
+}
+
+PhotonError PhotonFcu_ExecCmd_SetMode(const PhotonDynArrayOfCharMaxSize256* mode)
+{
+    return PhotonError_Ok;
+}
+
+#endif
 
 #undef _PHOTON_FNAME
