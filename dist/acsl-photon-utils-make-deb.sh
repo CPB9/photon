c5095c420c05220c516e7d95e4fa700019d0c2d3
Skorokhod Sergey <skorokhod.sergey@gmail.com>
2018-06-14 17:28:27 +0300

    deb-package scripts added

---
+++ dist/acsl-photon-utils-make-deb.sh
@@ -0,0 +1,2 @@
+hashdeep -c MD5 -l -o f -r ./acsl-photon-utils/opt/ > ./acsl-photon-utils/DEBIAN/md5sums
+fakeroot dpkg-deb --build acsl-photon-utils
---
+++ dist/acsl-photon-utils-prepare-deb.sh
@@ -0,0 +1,10 @@
+build_path=../build
+dest_path=/opt/acsl/
+pkg_path=./acsl-photon-utils/$dest_path
+
+mkdir -p $pkg_path/bin/
+cp ../build/photon-client $pkg_path/bin/
+cp ../build/photon-client-serial $pkg_path/bin/
+cp ../build/photon-client-udp $pkg_path/bin/
+strip -s $pkg_path/bin/*
+
